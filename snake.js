2ec59dd7fe70d9286d9999de36ce9949112e4f6c
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-01-26 16:10:19 -0800

    create grid

--- snake.js
+++ snake.js
@@ -1,3 +1,25 @@
 const grid = document.querySelector(".grid");
 const startBtn = document.getElementById("start");
-const scroreEL = document.getElementById("score");
+const scoreEL = document.getElementById("score");
+const gridItems = [];
+
+createGrid();
+
+function createGrid(){
+    
+    
+
+    // create divs
+    for(let i=0;i<100;i++){
+        // create div
+        const square = document.createElement('div');
+        // console.log(square);
+        // add styling to each square
+        square.classList.add("square");
+        // add div to grid
+        grid.appendChild(square);
+        // add each new square into array
+        gridItems.push(square);
+       
+    }
+}
